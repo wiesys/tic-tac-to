144313d9b4dcaf5c197443f089e4ae5c845877ff
Viesturs Knopkens <wiesys@gmail.com>
2024-01-23 14:56:51 +0200

    pirmā versija gudrākam spēlētājam

--- script.js
+++ script.js
@@ -73,21 +73,87 @@ function parbauditUzvaru() {
 }
 
 function datoraGajiens() {
-  let brivieIndeksi = [];
+  let galds = [];
   document.querySelectorAll('.poga').forEach((poga, index) => {
-    if (poga.innerHTML === '&nbsp;') {
-      brivieIndeksi.push(index);
-    }
+    galds[index] = poga.innerHTML;
   });
-  const randomIndex = brivieIndeksi[Math.floor(Math.random() * brivieIndeksi.length)];
-  document.querySelectorAll('.poga')[randomIndex].innerText = xVaiO;
-  document.querySelectorAll('.poga')[randomIndex].disabled = true;
-  if (xVaiO === 'X') {
-    xVaiO = 'O';
-  } else {
-    xVaiO = 'X';
+
+  let gajiens = atrastLabakoGajienu(galds);
+  if (gajiens !== -1) {
+    document.querySelectorAll('.poga')[gajiens].innerText = xVaiO;
+    document.querySelectorAll('.poga')[gajiens].disabled = true;
+    xVaiO = xVaiO === 'X' ? 'O' : 'X';
   }
+
   parbauditUzvaru();
+}
+
+function atrastLabakoGajienu(galds) {
+  // Vai varam uzvarēt?
+  let winMove = findWinningMove(galds, xVaiO);
+  if (winMove !== -1) return winMove;
+
+  // Vai varam nobloķēt?
+  let opponent = xVaiO === 'X' ? 'O' : 'X';
+  let blockMove = findWinningMove(galds, opponent);
+  if (blockMove !== -1) return blockMove;
+
+  // Centrs
+  if (galds[4] === '&nbsp;') return 4;
+
+  // Pretējie stūri vai tukši stūri
+  let corners = [0, 2, 6, 8];
+  let move = findCornerMove(galds, corners);
+  if (move !== -1) return move;
+
+  // Tukšas malas
+  let sides = [1, 3, 5, 7];
+  for (let side of sides) {
+    if (galds[side] === '&nbsp;') return side;
+  }
+
+  // Nekā nav
+  return -1;
+}
+
+function findWinningMove(galds, player) {
+  for (let i = 0; i < 9; i++) {
+    if (galds[i] === '&nbsp;') {
+      galds[i] = player;
+      if (isWinning(galds, player)) {
+        galds[i] = '&nbsp;';
+        return i;
+      }
+      galds[i] = '&nbsp;';
+    }
+  }
+  return -1;
+}
+
+function isWinning(galds, player) {
+  return uzvarasIespejas.some((combination) => {
+    return combination.every((index) => {
+      return galds[index] === player;
+    });
+  });
+}
+
+function findCornerMove(galds, corners) {
+  let opponent = xVaiO === 'X' ? 'O' : 'X';
+  for (let corner of corners) {
+    if (galds[corner] === '&nbsp;') {
+      // Opposite corner
+      let oppositeCorner = 8 - corner;
+      if (galds[oppositeCorner] === opponent) return corner;
+    }
+  }
+
+  // Any empty corner
+  for (let corner of corners) {
+    if (galds[corner] === '&nbsp;') return corner;
+  }
+
+  return -1;
 }
 
 const pogas = document.querySelectorAll('.poga');
